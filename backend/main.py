526318106a97a187e57417474c05a74258b64fe4
Dawid Szewc <xephen20@gmail.com>
2024-06-13 19:04:04 +0200

    Improving main

--- backend/main.py
+++ backend/main.py
@@ -1,68 +1,92 @@
-from flask import Flask, Response, stream_with_context
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-import threading, argparse
-import cv2
-from processing.camera import Camera, ImageProcessor, VideoStreamer
-from processing.robot import Fanuc
+import argparse
+import json
+import subprocess
 
-lock = threading.Lock()
-grayFrame = None
+# Load configurations
+with open("data/params.json") as file:
+    g_params = json.load(file)
 
+# Flask
 app = Flask(__name__)
-CORS(app)
-
-# Global instances
-camera = Camera()
-robot = Fanuc(ip="192.168.125.100")
-processor = ImageProcessor(
-    calib_location="/home/pi/camera_interface/calib.p", robot=robot
-)
-video_streamer = VideoStreamer(camera, processor, lock)
+CORS(app)  # allow foreign origin
 
 
-def stream_frames():
-    while True:
-        frame = video_streamer.get_frame()
-        if frame is None:
-            continue
-        (flag, encodedImage) = cv2.imencode(".jpg", frame)
-        if not flag:
-            continue
-        yield (
-            b"--frame\r\n"
-            b"Content-Type: image/jpeg\r\n\r\n" + bytearray(encodedImage) + b"\r\n"
-        )
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
-@app.route("/feed_gray")
-def feed_gray():
-    return Response(
-        stream_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
-    )
+@app.route("/api/gray", methods=["PUT"])
+def update_gray():
+    with open("data/params.json") as file:
+        params = json.load(file)
+    params["gray"] = request.json
+    g_params["gray"] = request.json
+
+    with open("/home/pi/params.json", "w") as file:
+        json.dump(params, file)
+    return jsonify(request.json)
 
 
-def main(runlevel):
-    streamer_thread = threading.Thread(target=video_streamer.start)
-    streamer_thread.daemon = True
-    streamer_thread.start()
+@app.route("/api/threshold", methods=["PUT"])
+def update_threshold():
+    with open("data/params.json") as file:
+        params = json.load(file)
+    params["threshold"] = request.json
+    g_params["threshold"] = request.json
+
+    with open("/home/pi/params.json", "w") as file:
+        json.dump(params, file)
+    return jsonify(request.json)
+
+
+@app.route("/api/params", methods=["GET"])
+def get_params():
+    with open("data/params.json") as file:
+        params = json.load(file)
+    return jsonify(params)
+
+
+@app.route("/api/cardboard_data", methods=["GET"])
+def cardboardManager():
+    with open("data/cardboard_data.json") as file:
+        params = json.load(file)
+    return jsonify(params)
+
+
+@app.route("/api/action", methods=["POST"])
+def do_action():
+    action = request.json.get("action")
+    if action == "restart":
+        subprocess.run(["supervisorctl", "stop", "camera"])
+        subprocess.run(["reboot"])
+    elif action == "poweroff":
+        subprocess.run(["supervisorctl", "stop", "camera"])
+        subprocess.run(["poweroff"])
+    elif action == "program":
+        subprocess.run(["supervisorctl", "restart", "camera"])
+    elif action == "update":
+        subprocess.run(["git", "pull"])
+        subprocess.run(["./runme.sh"])
+    return jsonify(request.json)
 
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument(
-        "-i", "--ip", type=str, required=True, help="ip address of the device"
+        "-i", "--ip", type=str, required=True, help="IP address of the device"
     )
     ap.add_argument(
         "-o",
         "--port",
         type=int,
         required=True,
-        help="ephemeral port number of the server (1024 to 65535)",
+        help="Ephemeral port number of the server (1024 to 65535)",
     )
-    ap.add_argument("-r", "--runlevel", type=int, default=1, help="runlevel")
+    ap.add_argument("-r", "--runlevel", type=int, default=1, help="Runlevel")
     args = vars(ap.parse_args())
-
-    main(args["runlevel"])
     app.run(
         host=args["ip"],
         port=args["port"],
@@ -70,4 +94,3 @@ if __name__ == "__main__":
         threaded=True,
         use_reloader=False,
     )
-    camera.stop_grabbing()
